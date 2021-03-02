9e08efcb2e1d15a370a9d546e84ee52fafa21f1c
Arham1019 <79666205+Arham1019@users.noreply.github.com>
2021-03-02 14:06:00 +0500

    Removed all the excess

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,49 +2,41 @@ package com.company;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Main M = new Main();
-        M.extractingurl("http://books.toscrape.com/catalogue/category/books_1/index.html");
+        Main Main = new Main();
+        Main.extract_books_name("http://books.toscrape.com/catalogue/category/books_1/index.html");
     }
-    public void concatingurl(String[] names,int i) {
+    public void concat_urls(String[] books_names) {
         String url = new String("https://books.toscrape.com/catalogue/category/books/");
-        String[] parts = names;
-        String href="/index.html";
-        String url2 = null;
-        Document URL=null;
-        for ( i = 0; i < names.length; i++) {
-            parts[i] = names[i];
-        }
-        for ( i = 0; i < names.length; i++) {
-            url2 = url + parts[i];
-            if (i==0) { //System.out.println(" "+url + parts[i]); not to use the first url
+        String[] names = books_names;
+        String domain="/index.html";
+         for ( int i = 0; i < books_names.length; i++) {
+            String url2 = url + names[i];                        //concatinating the url with books name
+            if (i==0) {
+                  // not to use the first url
             }
             else {
-                  if (url2.endsWith(parts[i]))
+                  if (url2.endsWith(names[i]))    //checking the url
                   {
-                      url2=url2+href;
+                      url2=url2+domain;
                       try {
-                          URL= (Document) Jsoup.connect(url2).get();
-                          Elements navlist=URL.getElementsByClass("nav-list");
-                          //System.out.println(" "+navlist.text()); //For selecting the side bar
-                          //Now getting the main page
-                          Elements pro=URL.getElementsByClass("product_pod");
-                          Elements h3=pro.select("h3");
-                          Elements price=URL.getElementsByClass("product_price");
-                          Elements p=URL.getElementsByClass("price_color");
+                          Document URL= (Document) Jsoup.connect(url2).get();
+                          Elements product=URL.getElementsByClass("product_pod");
+                          Elements title=product.select("h3");
+                          Elements price=URL.getElementsByClass("price_color");
                           Elements avail=URL.getElementsByClass("instock availability");
-                          System.out.println(" "+parts[i]);
-                          System.out.println(" "+h3.text());
-                          System.out.println(" "+p.text());
-                          System.out.println(" "+avail.text());
-                          System.out.println(" ");
+                          System.out.println("Title:"+" "+names[i].split("_")[0]);   //gives the name
+                          System.out.println(" "+title.text());      //gives the title of the books
+                          System.out.println(" "+price.text());      //gives the price of the books
+                          System.out.println(" "+avail.text());      //gives the in stock or out of stock
+                          System.out.println(" ");              //just to give a blank space between two sets
                           System.out.println(" ");
                       } catch (Exception e) {
                           e.printStackTrace();
@@ -54,27 +46,23 @@ public class Main {
         }
 
     }
-    public void extractingurl(String href) throws IOException {
-        Document Doc=Jsoup.connect(href).get();
-        Elements list=Doc.getElementsByClass("nav-list");
-        Elements a=list.select("a");
-        String[] names=new String[a.size()];
-        int i;
-        String ul2;
-        for( i=0;i<a.size();i++)    //This loop gets all the url from navlist and stores it in names
+    public void extract_books_name(String url) throws IOException {
+        Document Doc=Jsoup.connect(url).get();
+        Elements nav_list=Doc.getElementsByClass("nav-list");
+        Elements a=nav_list.select("a");
+        String[] books_name=new String[a.size()];
+         for( int i=0;i<a.size();i++)    //This loop gets all the url from navlist and stores it in names
         {
-            if (names[i]=="0")
+            if (books_name[i]=="0")
             {
-                //Just to eliminate the first row as
+                return;
             }
             else if (i>0){
-
-                names[i] = a.get(i).attr("href"); //gives the list of the links
-                names[i]=names[i].split("/")[2];      //gives the name travel_2
-                //System.out.println(" " + names[i]);
+                books_name[i] = a.get(i).attr("href"); //gives the list of the links
+                books_name[i]=books_name[i].split("/")[2];      //gives the names of all the books
             }
         }
-        concatingurl(names,i);
+        concat_urls(books_name);
 }
 }
 
